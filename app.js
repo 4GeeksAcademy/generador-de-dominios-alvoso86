587245c68dae0b2d04f395c22cdd7203484560be
Yoalvarado25 <manacima2019@gmail.com>
2025-03-21 19:20:32 +0000

    dominio

---
+++ app.js
@@ -0,0 +1,12 @@
+let pronoun = ['the','your', 'our', 'his', 'she'];
+  let adj = ['great', 'big', 'amazing' ,'awesome'];
+  let noun = ['jogger', 'racoon', 'dog', 'rabbit', 'cat'];
+  
+  // generating the domains
+  for(let i = 0; i < pronoun.length; i++) {
+    for(let a = 0; a < adj.length; a++) {
+      for(let b = 0; b < noun.length; b++) {
+        console.log(pronoun[i] + adj[a] + noun[b]+'.com'); 
+      }
+    }
+  }
